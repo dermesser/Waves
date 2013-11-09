556a6bd70bc984e88fa8da4906eb4f71bd321708
lbo <lbo@spheniscida.de>
2013-11-09 22:12:25 +0100

    Better 2D plotter

--- Waves/Tableout.hs
+++ Waves/Tableout.hs
@@ -3,9 +3,14 @@ module Tableout where
 import Waves.Types
 import Waves.Math
 
+import Data.List
 import Text.Printf
 
 -- String suitable for gnuplot (use writeFile -- may take megabytes!)
 --                     Step width
 print2D :: WaveField -> Double -> Time -> String
-print2D w@(WaveField (Medium c xdim ydim) exciters) step t = [0,0+step..xdim] >>= \x -> [0,0+step..ydim] >>= \y -> show x ++ " " ++ show y ++ " " ++ printf "%.15f" (wave2d w (x,y) t) ++ "\n"
+print2D w@(WaveField (Medium c xdim ydim) exciters) step t = concatMap xfunc xlist
+    where xlist = [0,0+step..xdim]
+          ylist = [0,0+step..ydim]
+          xfunc x = (concatMap (\y -> yfunc x y) ylist) ++ "\n"
+          yfunc x y = show x ++ " " ++ show y ++ " " ++ printf "%.3f" (wave2d w (x,y) t) ++ "\n"
